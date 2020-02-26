810b4138dc311ee121158dc509428ccc2ea28b8a
porao <ratomorto@riseup.net>
2020-02-26 12:02:24 -0300

    call modal with Jquery

--- code.js
+++ code.js
@@ -2,23 +2,24 @@ const button = document.querySelector('button')
 const binary = document.getElementsByName('binary')[0]
 const decimal = document.getElementsByName('decimal')[0]
 
-button.onclick = function() {convertBinaryToDecimal() }
+button.onclick = function () { convertBinaryToDecimal() }
 
 function convertBinaryToDecimal() {
-    if(isBinary()) {
+    if (isBinary()) {
         let result = 0;
         const binaryValue = binary.value;
         let total = 0;
-        for(let i = 0; i < binaryValue.length; i++) {
-            result += parseInt(binaryValue[i]) * (Math.pow(2, binaryValue.length-i-1))
+        for (let i = 0; i < binaryValue.length; i++) {
+            result += parseInt(binaryValue[i]) * (Math.pow(2, binaryValue.length - i - 1))
         }
         decimal.value = result;
     }
     else {
-        alert(binary.value + " não é um numero Binário \n Digite apenas numeros entre 0 e 1");
+        //se não for binário, ativa o modal
+        $("#myModal").modal();
     }
-}
 
-function isBinary() {
-    return ([...binary.value].filter(a => !(a == 0 || a == 1))).length == 0
+    function isBinary() {
+        return ([...binary.value].filter(a => !(a == 0 || a == 1))).length == 0
+    }
 }
